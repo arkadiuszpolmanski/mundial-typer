7e2442bf8e1bd36195fdaf2afa4b5110f173a807
Arek <arkadiusz.polmanski@gmail.com>
2026-06-11 11:51:00 +0200

    Zmiana strefy czasowej

--- src/main/java/com/betatfamily/mundial_typer/MundialTyperApplication.java
+++ src/main/java/com/betatfamily/mundial_typer/MundialTyperApplication.java
@@ -2,11 +2,13 @@ package com.betatfamily.mundial_typer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MundialTyperApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Warsaw"));
 		SpringApplication.run(MundialTyperApplication.class, args);
 	}
 
